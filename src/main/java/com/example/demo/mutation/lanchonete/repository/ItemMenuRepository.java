453351745d7041074311db7dce343566e40dbeff
Ana Carolina Hermann <anah@db.tec.br>
2025-05-06 08:24:08 -0300

    fix para rodar em outras maquinas o_O

--- src/main/java/com/example/demo/mutation/lanchonete/repository/ItemMenuRepository.java
+++ src/main/java/com/example/demo/mutation/lanchonete/repository/ItemMenuRepository.java
@@ -2,9 +2,12 @@ package com.example.demo.mutation.lanchonete.repository;
 
 import com.example.demo.mutation.lanchonete.entidade.ItemMenu;
 
-// Dados do banco são mocados nos testes
+import org.springframework.stereotype.Component;
+
+@Component
 public class ItemMenuRepository {
 
+  // Dados do banco são mocados durante os testes
   public ItemMenu getItem(String nome) {
     return null;
   }
